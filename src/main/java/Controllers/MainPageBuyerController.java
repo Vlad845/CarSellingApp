cece668ad639291a5fd6c5a14d89e73846cd6f4c
Vlad845 <vlad.iacoblev@gmail.com>
2021-05-15 12:35:42 +0300

    the search bar works but not on its full potential

--- src/main/java/Controllers/MainPageBuyerController.java
+++ src/main/java/Controllers/MainPageBuyerController.java
@@ -3,8 +3,12 @@ package Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -35,7 +39,19 @@ public class MainPageBuyerController {
     }
 
     @FXML
-    void searchButtonAction(ActionEvent event) {
+    void searchButtonAction(ActionEvent event) throws IOException {
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/Search.fxml"));
+
+        BorderPane search = fxmlLoader.load();
+        mainPagePane.setCenter(search);
+
+
+
 
     }
+
+
 }
